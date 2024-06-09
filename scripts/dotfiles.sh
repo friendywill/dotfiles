a44862e70be067ed5d9bae174b1d7ebf525b2bf8
friendywill <will@friendy.dev>
2024-06-09 11:19:11 +1000

    Added dotfiles script with intention to replace two scripts

---
+++ scripts/dotfiles.sh
@@ -0,0 +1,16 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/ohmyzsh/ohmyzsh.git https://github.com/ohmyzsh/ohmyzsh.git
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+source ~/.zshrc
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+ln -s "$SCRIPT_DIR/../.p10k.zsh" ~/.p10k.zsh
+ln -s "$SCRIPT_DIR/../.tmux" ~/.tmux
+ln -s "$SCRIPT_DIR/../.vim" ~/.vim
+ln -s "$SCRIPT_DIR/../.zprofile" ~/.zprofile
+ln -s "$SCRIPT_DIR/../.zshrc" ~/.zshrc
+ln -s "$SCRIPT_DIR/../.tmux.conf" ~/.tmux.conf
+ln -s "$SCRIPT_DIR/../.config/" ~/.config
+ln -s "$SCRIPT_DIR/../.oh-my-zsh/" ~/.oh-my-zsh
+ln -s "$SCRIPT_DIR/../.gitconfig" ~/.gitconfig
