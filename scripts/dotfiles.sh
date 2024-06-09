81189081e6f33f2672af9fade9112beb9b7d34f9
friendywill <will@friendy.dev>
2024-06-09 12:08:51 +1000

    Skipping oh-my-zsh setup

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -8,7 +8,7 @@ ln -s "$SCRIPT_DIR/../.zshrc" ~/.zshrc
 ln -s "$SCRIPT_DIR/../.tmux.conf" ~/.tmux.conf
 ln -s "$SCRIPT_DIR/../.config/" ~/.config
 ln -s "$SCRIPT_DIR/../.gitconfig" ~/.gitconfig
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh/
+git clone https://github.com/ohmyzsh/ ~/.oh-my-zsh/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
