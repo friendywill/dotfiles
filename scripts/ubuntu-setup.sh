9da790655d0e77ce350619b880945125e4f8b473
friendywill <will@friendy.dev>
2024-06-09 16:30:13 +1000

    Added Neovim installer for ubuntu

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -2,4 +2,8 @@
 
 apt update
 apt upgrade -y
-apt install curl git speedtest-cli wget zsh -y
+apt install curl git speedtest-cli wget zsh snapd -y
+systemctl unmask snapd.service
+systemctl enable snapd.service
+systemctl start snapd.service
+snap install nvim --classic
