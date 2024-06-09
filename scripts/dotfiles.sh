ffe881357c395a3e8a16067301ce445e4a6913af
friendywill <will@friendy.dev>
2024-06-09 12:16:05 +1000

    Remove install script for oh my zsh

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,4 +1,3 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s "$SCRIPT_DIR/../.p10k.zsh" ~/.p10k.zsh
 ln -s "$SCRIPT_DIR/../.tmux" ~/.tmux
@@ -8,7 +7,7 @@ ln -s "$SCRIPT_DIR/../.zshrc" ~/.zshrc
 ln -s "$SCRIPT_DIR/../.tmux.conf" ~/.tmux.conf
 ln -s "$SCRIPT_DIR/../.config/" ~/.config
 ln -s "$SCRIPT_DIR/../.gitconfig" ~/.gitconfig
-git clone https://github.com/ohmyzsh/ ~/.oh-my-zsh/
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
