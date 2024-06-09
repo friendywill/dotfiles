4ca50b3d0a78ed6acdeeace8673b1d63f18a8477
friendywill <will@friendy.dev>
2024-06-09 12:21:26 +1000

    Moved script dir to correct location within dotfiles script

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,3 +1,4 @@
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s "$SCRIPT_DIR/../.p10k.zsh" ~/.p10k.zsh
 ln -s "$SCRIPT_DIR/../.tmux" ~/.tmux
@@ -9,7 +10,5 @@ ln -s "$SCRIPT_DIR/../.config/" ~/.config
 ln -s "$SCRIPT_DIR/../.gitconfig" ~/.gitconfig
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 source ~/.zshrc
