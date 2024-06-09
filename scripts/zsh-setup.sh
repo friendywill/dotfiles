58a9f26b4e2263355409f6e242241b23b6272d26
friendywill <will@friendy.dev>
2024-06-09 10:01:43 +1000

    Trying fix for zsh setup script

--- scripts/zsh-setup.sh
+++ scripts/zsh-setup.sh
@@ -1,5 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-curl https://github.com/ohmyzsh/ohmyzsh/raw/master/oh-my-zsh.sh -o ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/oh-my-zsh.sh}
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom/themes/powerlevel10k
+curl https://github.com/ohmyzsh/ohmyzsh/raw/master/oh-my-zsh.sh -o $ZSH_CUSTOM:-$HOME/.oh-my-zsh/oh-my-zsh.sh
 source ~/.zshrc
