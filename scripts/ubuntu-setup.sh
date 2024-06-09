a53aea5ebe3ba224a3ba4c2fd4910bd0fcd23748
friendywill <will@friendy.dev>
2024-06-09 16:49:41 +1000

    Added nerd font installation

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -7,3 +7,9 @@ systemctl unmask snapd.service
 systemctl enable snapd.service
 systemctl start snapd.service
 snap install nvim --classic
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hermit.zip &&
+	cd ~/.local/share/fonts &&
+	unzip Hermit.zip &&
+	rm Hermit.zip &&
+	fc-cache -fv &&
+	cd ~/
