2cab5c6ede0e5f4e030e3c52f4b7494361e01d78
friendywill <will@friendy.dev>
2024-06-09 10:24:46 +1000

    Using oh-my-zsh curl command

--- scripts/zsh-setup.sh
+++ scripts/zsh-setup.sh
@@ -1,5 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/oh-my-zsh.sh -o $HOME/.oh-my-zsh/oh-my-zsh.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 source ~/.zshrc
