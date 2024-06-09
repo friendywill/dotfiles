990a4dbbdff7fe0b35d0629524d5cc77c34b833b
friendywill <will@friendy.dev>
2024-06-09 12:03:28 +1000

    Reordering script

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,10 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh/
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-source ~/.zshrc
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 ln -s "$SCRIPT_DIR/../.p10k.zsh" ~/.p10k.zsh
 ln -s "$SCRIPT_DIR/../.tmux" ~/.tmux
 ln -s "$SCRIPT_DIR/../.vim" ~/.vim
@@ -12,5 +7,10 @@ ln -s "$SCRIPT_DIR/../.zprofile" ~/.zprofile
 ln -s "$SCRIPT_DIR/../.zshrc" ~/.zshrc
 ln -s "$SCRIPT_DIR/../.tmux.conf" ~/.tmux.conf
 ln -s "$SCRIPT_DIR/../.config/" ~/.config
-ln -s "$SCRIPT_DIR/../.oh-my-zsh/" ~/.oh-my-zsh
 ln -s "$SCRIPT_DIR/../.gitconfig" ~/.gitconfig
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+source ~/.zshrc
