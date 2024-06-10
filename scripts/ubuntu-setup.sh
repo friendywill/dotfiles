008cdba150bf357d4a5319ccfb1e2e397549e87e
friendywill <will@friendy.dev>
2024-06-10 11:52:09 +1000

    added root permission to install lazygit

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -19,6 +19,6 @@ wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/do
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-install lazygit /usr/local/bin
+sudo install lazygit /usr/local/bin
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 zsh $SCRIPT_DIR/dotfiles.sh
