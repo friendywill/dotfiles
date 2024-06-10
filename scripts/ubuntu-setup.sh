d4ba412ff8d121817fd69a89f51eb4285e9fee00
friendywill <will@friendy.dev>
2024-06-10 11:02:45 +1000

    Finished off ubuntu script

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
-apt install curl git speedtest-cli ripgrep fd-find wget zsh snapd -y
-systemctl unmask snapd.service
-systemctl enable snapd.service
-systemctl start snapd.service
-snap install nvim --classic
+sudo apt update
+sudo apt upgrade -y
+sudo apt install curl git speedtest-cli ripgrep fd-find wget zsh snapd -y
+sudo systemctl unmask snapd.service
+sudo systemctl enable snapd.service
+sudo systemctl start snapd.service
+sudo snap install nvim --classic
 wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hermit.zip &&
 	cd ~/.local/share/fonts &&
 	unzip Hermit.zip &&
