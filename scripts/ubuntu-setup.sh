c7c44e37fcd6f7bd89fde89b662142fd7454e3fb
friendywill <will@friendy.dev>
2024-06-11 21:11:47 +1000

    added some useful zsh plugins

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -18,6 +18,10 @@ wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/do
 	cd ~/
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/mafredri/zsh-async ~/.oh-my-zsh/plugins/async
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
