6f3ed635ff30074b251d2d8279eb21a8dc6dff4a
friendywill <will@friendy.dev>
2024-08-13 18:54:02 +0000

    Updated lazyvim and added official docker repo

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -2,6 +2,17 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install ripgrep fzf gcc curl git speedtest-cli ripgrep fd-find wget zsh snapd -y
+# Add Docker's official GPG key:
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+	sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo systemctl unmask snapd.service
 sudo systemctl enable snapd.service
 sudo systemctl start snapd.service
