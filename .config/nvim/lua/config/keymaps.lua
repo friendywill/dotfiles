d9e6d28812137b36f89543f63f952d0fd26bffc9
friendywill <will@friendy.dev>
2024-08-13 19:33:40 +0000

    Added lazydocker keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- docker
+if vim.fn.executable("lazydocker") == 1 then
+  vim.keymap.set("n", "<leader>gd", function()
+    LazyVim.terminal("lazydocker", { esc_esc = false, ctrl_hjkl = false })
+  end, { desc = "Lazydocker" })
+end
