2c0fa664bc79d3cc43f50e8d0a55b902e0205c81
friendywill <will@friendy.dev>
2024-08-13 19:33:53 +0000

    added service docker start

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -15,6 +15,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo service docker start
 sudo systemctl unmask snapd.service
 sudo systemctl enable snapd.service
 sudo systemctl start snapd.service
