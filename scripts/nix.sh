1d7447685e477b3eb5c7b8859b4a29034f879105
friendywill <will@friendy.dev>
2024-09-09 19:13:54 +1000

    added config in nix script

--- scripts/nix.sh
+++ scripts/nix.sh
@@ -2,4 +2,13 @@ curl -L https://nixos.org/nix/install | sh -s -- --daemon
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
-echo 'export ZDOTDIR="$HOME/.config/zsh"' | tee -a /etc/zsh/zshenv > /dev/null
+
+git clone https://github.com/tmux-plugins/tpm $XDG_CONFIG_HOME/tmux/plugins/tpm
+mkdir ~/.config/
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.config/* ~/.config
+git clone https://github.com/ohmyzsh/ohmyzsh.git $XDG_DATA_HOME/oh-my-zsh/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $XDG_DATA_HOME/.oh-my-zsh/custom/themes/powerlevel10k
+echo 'export ZDOTDIR="$HOME"/.config/zsh' | sudo tee -a /etc/zsh/zshenv > /dev/null
+chsh -s $(which zsh)
+zsh
