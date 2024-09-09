0b14a439f0c07f8b8f3606b2438908ec3545192a
friendywill <will@friendy.dev>
2024-09-09 20:10:26 +1000

    Fixing NixOS

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,10 +1,11 @@
 { config, pkgs, ... }:
 
+let
+  username = builtins.getEnv "USER";
+in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "${USER}";
-  home.homeDirectory = "/home/${USER}";
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
