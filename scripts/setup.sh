c0ead1ebc4d915be606c02a41cf21c8cf304ce13
friendywill <will@friendy.dev>
2024-09-28 09:33:23 +1000

    Ensured symlink is created for zsh when installing with homebrew

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,5 +2,6 @@ sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply friendywill
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 export PATH=/home/linuxbrew/.linuxbrew/bin/:$PATH
 brew bundle --file=~/.local/share/chezmoi/Brewfile
+sudo ln -s $(which zsh) /bin/zsh
 chsh -s $(which zsh)
 zsh
