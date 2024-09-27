48efdaaf276d7413d12947a437e665b64232af19
friendywill <will@friendy.dev>
2024-09-28 09:59:41 +1000

    Added some doc comments

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,6 @@
+# Apply my dotfiles config using the chezmoi manager
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply friendywill
+# Installing homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 export PATH=/home/linuxbrew/.linuxbrew/bin/:$PATH
 brew bundle --file=~/.local/share/chezmoi/Brewfile
