6f0b7e3cd2c7837214da1fc6683659dbe67a2f75
friendywill <will@friendy.dev>
2024-10-20 09:37:23 +1100

    Added the signs back into git signs, cleaned up the function as well

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -7,6 +7,24 @@ local keymaps = require("keymaps")
 return {
   {
     "lewis6991/gitsigns.nvim",
-    opts = keymaps.set_git_sign_keys(),
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+        keymaps.set_git_sign_keys(map, gitsigns)
+      end,
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
   },
 }
