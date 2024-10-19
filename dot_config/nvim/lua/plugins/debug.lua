a69495d667e55075686de90f2cced48905873fb4
friendywill <will@friendy.dev>
2024-10-19 21:51:23 +1100

    Removed old note

--- dot_config/nvim/lua/plugins/debug.lua
+++ dot_config/nvim/lua/plugins/debug.lua
@@ -8,9 +8,7 @@
 local keymaps = require("keymaps")
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
   "mfussenegger/nvim-dap",
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
     "rcarriga/nvim-dap-ui",
