b4cbc3b7e337a9ea993a6de675bf893639e09f61
friendywill <will@friendy.dev>
2024-10-24 10:30:18 +1100

    Fixing healthchecks for noice

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -16,6 +16,13 @@ return {
   config = function()
     local noice = require("noice")
     noice.setup({
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
+      },
       routes = {
         {
           view = "cmdline",
