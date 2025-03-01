e9a060d2647faa8358f7d983170e9c1e06732b81
friendywill <will@friendy.dev>
2025-03-01 11:15:36 +1100

    feat(neovim): use ts_ls instead of vtsls

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -83,7 +83,6 @@ return {
           },
         },
       },
-      vtsls = {},
       isort = {},
       markdownlint = {},
       marksman = {},
@@ -101,6 +100,11 @@ return {
           },
         },
       },
+        ts_ls = {
+            root_dir = require("lspconfig").util.root_pattern({ "package.json", "tsconfig.json" }),
+            single_file_support = false,
+            settings = {},
+        },
         denols = {
             root_dir = require("lspconfig").util.root_pattern({"deno.json", "deno.jsonc"}),
             single_file_support = false,
