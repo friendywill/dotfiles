1e2ac3e167fc916c39a643017e877f0fd438c5b8
William Friend <will@friendy.dev>
2025-04-28 11:36:24 +1000

    feat(neovim): use autopep8

--- dot_config/nvim/lua/plugins/autoformat.lua
+++ dot_config/nvim/lua/plugins/autoformat.lua
@@ -5,7 +5,7 @@ return { -- Autoformat
   cmd = { "ConformInfo" },
   keys = keymaps.set_af_keys(),
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     -- format_on_save = function(bufnr)
     --   -- Disable "format_on_save lsp_fallback" for languages that don't
     --   -- have a well standardized coding style. You can add additional
@@ -25,7 +25,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { "stylua" },
       -- Conform can also run multiple formatters sequentially
-      python = { "isort" },
+      python = { "autopep8", stop_after_first = true },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
