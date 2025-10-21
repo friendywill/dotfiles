1933c8d821de91ceb7ac16393df0c5ba0d6dba31
William Friend <will@friendy.dev>
2025-10-22 10:32:13 +1100

    feat: add kotlin formatting

--- dot_config/nvim/lua/plugins/autoformat.lua
+++ dot_config/nvim/lua/plugins/autoformat.lua
@@ -29,6 +29,7 @@ return { -- Autoformat
         python = { "isort", "autopep8" },
         html = { "prettierd" },
         yaml = { "yamlfmt" },
+        kotlin = { "ktfmt", "ktlint" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
