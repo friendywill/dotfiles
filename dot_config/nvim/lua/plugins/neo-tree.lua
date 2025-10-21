2a862eb3269d9187c0d72e24b7d5c6db7bdd8827
William Friend <will@friendy.dev>
2025-10-22 10:32:53 +1100

    feat: group empty dirs

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -13,7 +13,9 @@ return {
   cmd = "Neotree",
   keys = keymaps.set_neotree_keys(),
   opts = {
+    group_empty_dirs = true,
     filesystem = {
+      group_empty_dirs = true,
       window = {
         mappings = keymaps.set_neotree_keys_other(),
       },
