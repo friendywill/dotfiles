ccc7972433726c6a1821318a8da87977d33317fe
William Friend <will@friendy.dev>
2025-10-22 10:32:27 +1100

    feat: add kotlin debugger

--- dot_config/nvim/lua/plugins/debug.lua
+++ dot_config/nvim/lua/plugins/debug.lua
@@ -6,6 +6,7 @@
 -- be extended to other languages as well. That's why it's called
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 local keymaps = require("keymaps")
+local log = require("vim.lsp.log")
 
 return {
   "mfussenegger/nvim-dap",
@@ -83,5 +84,56 @@ return {
         detached = vim.fn.has("win32") == 0,
       },
     })
+    -- Kotlin Setup
+    dap.adapters.kotlin = {
+      type = "executable",
+      command = "kotlin-debug-adapter",
+      options = { auto_continue_if_many_stopped = false },
+    }
+
+    dap.configurations.kotlin = {
+      {
+        type = "kotlin",
+        request = "launch",
+        name = "This file",
+        -- may differ, when in doubt, whatever your project structure may be,
+        -- it has to correspond to the class file located at `build/classes/`
+        -- and of course you have to build before you debug
+        mainClass = function()
+          local fname = vim.api.nvim_buf_get_name(0)
+
+          -- Match everything after src/main/kotlin/
+          local class = fname:match("src/main/kotlin/(.*).kt$")
+          return class:gsub("/", ".") .. "Kt"
+          -- -- src/main/kotlin/websearch/Main.kt -> websearch.MainKt
+          -- log.info(fname:gsub(root, ""):gsub("main/kotlin/", ""):gsub(".kt", "Kt"):gsub("/", "."):sub(2, -1))
+          -- return fname:gsub(root, ""):gsub("main/kotlin/", ""):gsub(".kt", "Kt"):gsub("/", "."):sub(2, -1)
+        end,
+        projectRoot = "${workspaceFolder}/kotlin",
+        function()
+          vim.log("${workspaceFolder}/kotlin")
+        end,
+        jsonLogFile = "",
+        enableJsonLogging = false,
+        args = function()
+          local args_string = vim.fn.input("Arguments: ")
+          return vim.split(args_string, " +")
+        end,
+      },
+      {
+        -- Use this for unit tests
+        -- First, run
+        -- ./gradlew --info cleanTest test --debug-jvm
+        -- then attach the debugger to it
+        type = "kotlin",
+        request = "attach",
+        name = "Attach to debugging session",
+        port = 5005,
+        args = {},
+        projectRoot = vim.fn.getcwd,
+        hostName = "localhost",
+        timeout = 2000,
+      },
+    }
   end,
 }
