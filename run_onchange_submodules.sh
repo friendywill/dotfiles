5f33168efd5569e2edd0233379dcb846b05a6c29
friendywill <will@friendy.dev>
2026-02-06 16:42:23 +1100

    chore: remove clone script

--- run_onchange_submodules.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.local/share/oh-my-zsh/
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.local/share/oh-my-zsh/custom/themes/powerlevel10k
-git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
