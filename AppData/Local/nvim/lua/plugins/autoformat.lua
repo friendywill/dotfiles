9ec18e4e0967be8665376ed1aae365638c009c3c
friendywill <will@friendy.dev>
2026-04-23 16:24:23 +1000

    chore(neovim): add autoformat configuration

---
+++ AppData/Local/nvim/lua/plugins/autoformat.lua
@@ -0,0 +1,28 @@
+return { -- Autoformat
+  "stevearc/conform.nvim",
+  opts = {
+    default_format_opts = {
+      timeout_ms = 3000,
+      async = false,
+      quiet = false,
+      lsp_format = "fallback",
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "autopep8" },
+      html = { "prettierd" },
+      yaml = { "yamlfmt" },
+      kotlin = { "ktfmt", "ktlint" },
+      json = { "jq" },
+    },
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+      isort = {
+        command = "isort",
+        args = {
+          "-",
+        },
+      },
+    },
+  },
+}
